b82c6cdab9e9ca5ae23c80361f8c52136b8ae225
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-07-25 21:46:56 -0300

    Update LoginRouter.js

--- src/routers/LoginRouter.js
+++ src/routers/LoginRouter.js
@@ -10,12 +10,10 @@ router.post('/register', async (req, res) => {
   const UserData = req.body;
 
   try {
-    // Procurar o usuário pelo email no banco de dados
     const existingEmailUser = await UserModel.findOne({email: UserData.email});
     const existingPasswordUser = await UserModel.findOne({name: UserData.name});
 
     if (existingEmailUser) {
-      // Se o email já existe no banco de dados, retorne um erro informando que o usuário já está registrado
       return res.status(409).send('Email já está em uso');
     }
     
@@ -24,13 +22,11 @@ router.post('/register', async (req, res) => {
     }  
     
     else {
-      // Se o email ou senha não existe, crie o novo usuário
       const newUser = await UserModel.create(UserData);
       res.send(newUser);
     }
     
   } catch (error) {
-    // Em caso de erro, retorne uma resposta de erro com o status 500
     res.status(500).send('Ocorreu um erro ao criar o usuário');
   }
 });
